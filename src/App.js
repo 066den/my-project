8fa960bb6ecc28a7df9a3696eccf8aa06aa71f1b
Denis Melnikov <066den@gmail.com>
2019-09-09 16:48:11 +0300

    09.09

--- src/App.js
+++ src/App.js
@@ -3,19 +3,15 @@ import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Content from './components/Content/Content';
-import {BrowserRouter} from "react-router-dom";
+
 
 const App = (props) => {
 	return (
-		<BrowserRouter>
-			<div className='app-wrapper'>
-				<Header />
-				<Sidebar />		
-				<Content 
-					state={props.state} 
-					dispatch={props.dispatch} />
-			</div>
-		</BrowserRouter>
+		<div className='app-wrapper'>
+			<Header />
+			<Sidebar />		
+			<Content />
+		</div>
 	)
 }
 
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
+import * as serviceWorker from './serviceWorker';
 import store from "./redux/redux-store";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
  
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
 	ReactDOM.render(
 		<BrowserRouter>
-			<App 
-				state={state} 
-				dispatch={store.dispatch.bind(store)}/>
+			<Provider store="{store}">
+				<App />
+			</Provider>
+			
 		</BrowserRouter>, document.getElementById('root'));
 }
 
-rerenderEntireTree(store.getState());
+rerenderEntireTree();
 
 store.subscribe(() => {
-	let state = store.getState();
-	rerenderEntireTree(state);
+	rerenderEntireTree();
 });
 
 
